b644d4f7cf42f2a2c6a186eb4e5c0c6d05fe950a
SonOfKhaos <khanmdminhaj786@gmail.com>
2024-07-12 09:37:27 -0400

    Added changes for login error

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -63,9 +63,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR,'frontend/static'),
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
