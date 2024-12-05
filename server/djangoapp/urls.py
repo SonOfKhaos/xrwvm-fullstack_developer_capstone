9f9b5ea7019c917a5caffc6c5b754be9faf39a31
SonOfKhaos <khanmdminhaj786@gmail.com>
2024-12-05 08:52:38 -0500

    added react pages for dealer and review details

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,14 +14,16 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
     # path for Dealership Details
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     # path for Dealership Details state-wise
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for Dealership Details w.r.t. Dealer ID
     path(route='get_dealers/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    # path for dealer reviews view
+    # path for Dealre Details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    # path for Dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    # path for add a review view
+    # path for Add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
     # path for Car Details
     path(route='get_cars', view=views.get_cars, name ='getcars'),
